78325139013542db3c33b94642095c86c6ee4411
Starias22 <adedeezechiel@gmail.com>
2024-08-24 23:13:56 -0400

    Format emotions as JSON
    Update emotion_detector function by formatting request results as JSON and extending emotions with  most dominant emotion\nReturn formatted emotions\nTest function with Python Shell

--- emotion_detection.py
+++ emotion_detection.py
@@ -1,3 +1,4 @@
+import json
 import requests
 
 def emotion_detector(text_to_analyse):
@@ -8,4 +9,10 @@ def emotion_detector(text_to_analyse):
     }
     myobj= { "raw_document": { "text": text_to_analyse } }
     response = requests.post(url, json = myobj, headers=headers)
-    return response.text
+    result=response.text
+    formatted_result = json.loads(result)
+    emotions=formatted_result["emotionPredictions"][0]["emotion"]
+    dominant_emotion=max(emotions,key=emotions.get)
+    emotions["dominant_emotion"]=dominant_emotion
+    return emotions
+    
