979e5a15287010bd88ec9aa3dbd8e05467a1fa1c
Starias22 <adedeezechiel@gmail.com>
2024-08-25 02:16:24 -0400

    Run static code analysis on server.py

--- server.py
+++ server.py
@@ -1,18 +1,21 @@
+''' This is the servr file, the entry point of application.
+It handle client requests for detecting emotion in texts
+'''
 from flask import Flask, request, render_template
 from EmotionDetection.emotion_detection import emotion_detector
-
 app=Flask("Emotion Detector")
-
 @app.route("/emotionDetector")
-def emotionDetector():
+def detect_emotion():
+    """ This is the core function of our web application.
+     It allow clients to enter text and perform emotion detection task.
+     """
     text_to_analyze=request.args.get("textToAnalyze")
     emotions=emotion_detector(text_to_analyze)
     dominant_emotion=emotions['dominant_emotion']
     if dominant_emotion is None:
         return "<b>Invalid text! Please try again!</b>"
     return f"For the given statement, the system response is {emotions}.\
-     The dominant emotion is <b>{dominant_emotion}</b>." 
-
+    The dominant emotion is <b>{dominant_emotion}</b>."
 @app.route("/")
 def render_index_page():
     ''' This function initiates the rendering of the main application
